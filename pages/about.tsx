36adc177c687056329682b7b17d8de3c6ca09799
Nicole Lee <70028306+nnicolee@users.noreply.github.com>
2022-07-19 18:35:39 -0700

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,6 +7,8 @@ import { getDirectory, IDirectoryRow } from "@utils/notion/directory";
 import { NextPage } from "next";
 import Head from "next/head";
 import PairCoding from "@svg/pair-coding.svg";
+import MissionImage from "@svg/mission.svg"
+import Book from "@svg/book.svg"
 
 interface AboutProps {
     directoryEntries: IDirectoryRow[]
@@ -22,6 +24,52 @@ const About : NextPage<AboutProps> = ({ directoryEntries }) => {
             <Navbar />
             <Animate>
                 <main className="pt-4 md:px-6 px-4 w-full my-12 flex flex-col items-center">
+                <Book className="w-full"/>
+                <div className="my-6">
+                        <Animate.Element
+                            resetAfterTriggered={false}
+                            as="h1" 
+                            from={{ y: 60, opacity: 0 }}
+                            to={{ y: 0, opacity: 1 }}
+                            className="text-6xl font-bold text-white text-center">
+                                What is <span className="text-brand-purple-secondary">Code4Tomorrow</span> ?
+                        </Animate.Element>
+                        <Animate.Element
+                            resetAfterTriggered={false}
+                            as="p" 
+                            from={{ y: 90,  opacity: 0 }}
+                            to={{ y: 0, opacity: 1, delay: 0.15 }}
+                            className="text-lg !mt-3 text-medium-grey text-center max-w-4xl">
+                            Code 4 Tomorrow's uniqueness lies in the fact that we are entirely student-run, from the official website to merch design and finance management. Fiscally sponsored by Irvine LIGHTS, C4T is a 501(c)(3) non-profit organization that offers free coding classes to students around the globe, as well as community service opportunities to our members and teachers.
+                        </Animate.Element>
+                    </div>
+        
+                        <section className="md:[&>div]:w-1/2 md:space-x-6 w-screen flex md:flex-row flex-col-reverse p-3 sm:p-20 max-w-7xl justify-items-center">
+                            <div className="my-10">
+                                <Animate.Element
+                                    resetAfterTriggered={false}
+                                    as="h1" 
+                                    from={{ y: 60, opacity: 0 }}
+                                    to={{ y: 0, opacity: 1 }}
+                                    className="text-5xl font-bold text-white">
+                                        Our <span className="text-brand-purple-secondary">Mission</span>
+                                </Animate.Element>
+                                <br></br>
+                                <Animate.Element
+                                    resetAfterTriggered={false}
+                                    as="p" 
+                                    from={{ y: 90,  opacity: 0 }}
+                                    to={{ y: 0, opacity: 1, delay: 0.15 }}
+                                    className="text-lg !mt-3 text-medium-grey max-w-4xl">
+                                    C4T aims to provide a bridge that connects students worldwide to their future by linking professionals and executives to mentor students, and providing a dynamic, driven team of high school students to teach topics from the humanities to STEM. We aim to inspire the next generation to consider career paths in Computer Science or STEM.
+                                </Animate.Element>
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <MissionImage className="w-full"/>
+                            </div>
+                        </section>
+                
+
                     <div className="my-6">
                         <Animate.Element
                             resetAfterTriggered={false}
